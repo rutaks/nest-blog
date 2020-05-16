c8f5263367b7a5102a68a949cfe67328a17fc995
rutaks <rootsum.dev@gmail.com>
2020-05-16 08:15:58 +0200

    Setup Passport JWT auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserEntity } from 'src/entity/user.entity';
+import { JWTStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
-  providers: [AuthService],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    JwtModule.register({
+      secret: String(process.env.SECRET),
+      signOptions: {
+        expiresIn: 360000,
+      },
+    }),
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+  ],
+  providers: [AuthService, JWTStrategy],
   controllers: [AuthController],
+  exports: [PassportModule, JWTStrategy],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,19 +8,21 @@ import { RegisterDTO, LoginDTO } from 'src/dto/user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from 'src/entity/user.entity';
 import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserEntity) private userRepo: Repository<UserEntity>,
+    private jwtService: JwtService,
   ) {}
 
   async register(credentials: RegisterDTO) {
     try {
       const newUser = this.userRepo.create(credentials);
       await newUser.save();
-
-      return newUser;
+      const token = this.jwtService.sign({ username: newUser.username });
+      return { user: { ...newUser.toJSON(), token } };
     } catch (error) {
       if (error.code === '23505') {
         throw new ConflictException('Username has already been taken');
@@ -32,12 +34,12 @@ export class AuthService {
   async login({ email, password }: LoginDTO) {
     try {
       const foundUser = await this.userRepo.findOne({ where: { email } });
-      if (foundUser && (await foundUser.comparePassword(password))) {
-        return foundUser;
-      }
-
-      throw new UnauthorizedException('Invalid Credentials');
+      const isValid = await foundUser.comparePassword(password);
+      if (!isValid) throw new UnauthorizedException('Invalid Credentials');
+      const token = this.jwtService.sign({ username: foundUser.username });
+      return { user: { ...foundUser.toJSON(), token } };
     } catch (error) {
+      console.log(error);
       throw new InternalServerErrorException(error.message);
     }
   }
