a280173c74dbed5b0391be3961a408d78002e485
rutaks <rootsum.dev@gmail.com>
2020-05-17 06:35:19 +0200

    Refactored dotenv use

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
